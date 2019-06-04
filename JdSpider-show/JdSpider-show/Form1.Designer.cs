7967a05e5b5a558f6334edd9552b6563f3223006
noodlefighter <rongjiayu@gmail.com>
2019-06-04 11:56:22 +0800

    完善展示

--- JdSpider-show/JdSpider-show/Form1.Designer.cs
+++ JdSpider-show/JdSpider-show/Form1.Designer.cs
@@ -28,13 +28,364 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.model = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.brand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.color = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ratio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.resolution = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.refresh_rate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.luminance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.url = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.model,
+            this.brand,
+            this.price,
+            this.color,
+            this.ratio,
+            this.resolution,
+            this.refresh_rate,
+            this.luminance,
+            this.title,
+            this.url});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1172, 223);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "显示全部";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "执行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(266, 257);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 60);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "过滤";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "brand",
+            "model",
+            "color",
+            "ratio",
+            "resolution",
+            "refresh_rate",
+            "luminance"});
+            this.comboBox1.Location = new System.Drawing.Point(30, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(192, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(811, 257);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 60);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "价格过滤";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "<1800";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 28);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "执行";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(180, 32);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.IsValueShownAsLabel = true;
+            series2.Label = "#VALX:#VAL ";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(928, 328);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.comboBox2);
+            this.groupBox3.Controls.Add(this.chart1);
+            this.groupBox3.Location = new System.Drawing.Point(34, 323);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1150, 394);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "数量统计";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "brand",
+            "ratio",
+            "resolution",
+            "refresh_rate",
+            "luminance"});
+            this.comboBox2.Location = new System.Drawing.Point(48, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "列";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "等于";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "列";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(41, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 28);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "统计";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // model
+            // 
+            this.model.DataPropertyName = "model";
+            this.model.HeaderText = "model";
+            this.model.Name = "model";
+            this.model.ReadOnly = true;
+            // 
+            // brand
+            // 
+            this.brand.DataPropertyName = "brand";
+            this.brand.HeaderText = "brand";
+            this.brand.Name = "brand";
+            this.brand.ReadOnly = true;
+            this.brand.Width = 50;
+            // 
+            // price
+            // 
+            this.price.DataPropertyName = "price";
+            this.price.HeaderText = "price";
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            // 
+            // color
+            // 
+            this.color.DataPropertyName = "color";
+            this.color.HeaderText = "color";
+            this.color.Name = "color";
+            this.color.ReadOnly = true;
+            // 
+            // ratio
+            // 
+            this.ratio.DataPropertyName = "ratio";
+            this.ratio.HeaderText = "ratio";
+            this.ratio.Name = "ratio";
+            this.ratio.ReadOnly = true;
+            // 
+            // resolution
+            // 
+            this.resolution.DataPropertyName = "resolution";
+            this.resolution.HeaderText = "resolution";
+            this.resolution.Name = "resolution";
+            this.resolution.ReadOnly = true;
+            // 
+            // refresh_rate
+            // 
+            this.refresh_rate.DataPropertyName = "refresh_rate";
+            this.refresh_rate.HeaderText = "refresh_rate";
+            this.refresh_rate.Name = "refresh_rate";
+            this.refresh_rate.ReadOnly = true;
+            // 
+            // luminance
+            // 
+            this.luminance.DataPropertyName = "luminance";
+            this.luminance.HeaderText = "luminance";
+            this.luminance.Name = "luminance";
+            this.luminance.ReadOnly = true;
+            // 
+            // title
+            // 
+            this.title.DataPropertyName = "title";
+            this.title.HeaderText = "title";
+            this.title.Name = "title";
+            this.title.ReadOnly = true;
+            // 
+            // url
+            // 
+            this.url.DataPropertyName = "url";
+            this.url.HeaderText = "url";
+            this.url.Name = "url";
+            this.url.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1208, 745);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "jdspider采集结果展示";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn model;
+        private System.Windows.Forms.DataGridViewTextBoxColumn brand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn color;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ratio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn resolution;
+        private System.Windows.Forms.DataGridViewTextBoxColumn refresh_rate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn luminance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn url;
     }
 }
 
--- JdSpider-show/JdSpider-show/Form1.cs
+++ JdSpider-show/JdSpider-show/Form1.cs
@@ -7,14 +7,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace JdSpider_show
 {
     public partial class Form1 : Form
     {
+        MySqlConnection con = new MySqlConnection(@"server=localhost;database=jdspider;user id=root;pwd=root");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+        }
+
+        private void do_show_in_table(string sql)
+        {
+            MySqlDataAdapter data = new MySqlDataAdapter(sql, con);
+            DataSet ds = new DataSet();            
+            data.Fill(ds);
+            con.Close();
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM data";
+            do_show_in_table(sql);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM data WHERE " + comboBox1.Text + "='" + textBox1.Text + "'";
+            do_show_in_table(sql);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM data WHERE price" + textBox2.Text;
+            do_show_in_table(sql);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string colname = comboBox2.Text;
+            string sql = "SELECT * FROM data WHERE " + colname + "<>''";
+
+            DataSet ds = new DataSet();
+            try
+            {
+                MySqlDataAdapter data = new MySqlDataAdapter(sql, con);
+                
+                data.Fill(ds);
+                con.Close();
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch { return; }
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                string colvalue = row[colname].ToString();
+                if (result.ContainsKey(colvalue))
+                {
+                    result[colvalue]++;
+                }
+                else
+                {
+                    result.Add(colvalue, 1);
+                }
+            }
+            
+            List<KeyValuePair<string, int>> lst = new List<KeyValuePair<string, int>>(result);
+            lst.Sort(delegate (KeyValuePair<string, int> s1, KeyValuePair<string, int> s2) {
+                return s2.Value.CompareTo(s1.Value);
+            });
+
+            chart1.Series[0]["PieLabelStyle"] = "Outside";//将文字移到外侧
+            chart1.Series[0]["PieLineColor"] = "Black";//绘制黑色的连线。
+            chart1.Series[0].XValueType = ChartValueType.String;
+
+            Dictionary<string, int> show_result = new Dictionary<string, int>();
+            for (int i = 0;i < lst.Count; i++)
+            {
+                if (i <= 10)
+                {
+                    show_result.Add(lst[i].Key, lst[i].Value);
+                }
+                else
+                {
+                    if (show_result.ContainsKey("其他"))
+                        show_result["其他"]++;
+                    else
+                        show_result.Add("其他", 0);
+                }
+            }
+
+            chart1.Series[0].Points.DataBindXY(show_result.Keys.ToList<string>(), show_result.Values.ToList<int>());
         }
     }
 }
